f65210d2813324c844a650044178745dcb845915
Daniel Parker <danielparker@live.com.au>
2020-09-16 14:48:41 +1000

    Relabel parameters and update for newer fitkit
    Parameter labels have changed so as to be consistent with other
    resonance models in qutilities.
    
    The code has also been updated to reflect recent changes in fitkit and
    now uses xarray.DataArray as the call data type. The new call type is
    advantageous because it allows the user to store metadata in the NetCDF
    files it can write to. In particular, we store the parameter values used
    ot generate data from the reflection model.

--- qutilities/reflection.py
+++ qutilities/reflection.py
@@ -8,7 +8,7 @@ models and tools for fitting the resonance parameters of a reflection-type reson
 import warnings
 
 import numpy as np
-import pandas as pd
+import xarray as xr
 import sympy as sp
 
 from .circle import *
@@ -16,73 +16,76 @@ from  qutilities import *
 
 from fitkit import Parametric1D
 
-def ideal_reflection(b_Qi = (3, 4.5, 6),
-                     b_Qc = (3, 4.5, 6),
+def ideal_reflection(b_Qi = (1000, 10000, 1000000),
+                     b_Qc = (1000, 10000, 1000000),
                      b_theta = (-np.pi, 0, np.pi),
                      b_phi = (-np.pi, 0, np.pi),
-                     b_fr = (1e9, 5e9, 11e9)):
+                     b_fc = (1e9, 5e9, 11e9)):
     """ returns a Parametric1D model for an ideal notch resonator
 
     Params:
-        Qi:     The log10 of the internal quality factor
-        Qc:     The log10 of the modulus of the complex coupling quality factor
-        theta:  The argument of the complex coupling quality factor
-        fr:     The resonance frequency
+        Qi:     The internal quality factor
+        Qc:     The modulus of the complex coupling quality factor
+        phi:    The argument of the complex coupling quality factor
+        theta:  A phase correction factor
+        fc:     The resonance frequency
 
     Args:   Parameter bounds as required by Parametric1D
     """
-    Qi, Qc, theta, Kc, Ki, phi, fr, f = sp.symbols('Qi Qc theta Kc Ki phi fr f')
+    Qi, Qc, theta, Kc, Ki, phi, fc, f = sp.symbols('Qi Qc theta Kc Ki phi fc f')
 
-    s11 = ((Kc - Ki) + 2j*(f - fr))/((Kc + Ki) - 2j*(f - fr))
-    expr = s11.subs(Kc, fr/((10**Qc)*sp.exp(1j*theta))).subs(Ki, fr/(10**Qi))
-    expr *= sp.exp(1j*phi)
+    s11 = ((Kc - Ki) + 2j*(f - fc))/((Kc + Ki) - 2j*(f - fc))
+    expr = s11.subs(Kc, fc/(Qc*sp.exp(1j*phi))).subs(Ki, fc/Qi)
+    expr *= sp.exp(1j*theta)
 
-    params = {'Qi': b_Qi, 'Qc': b_Qc, 'theta': b_theta, 'phi': b_phi, 'fr': b_fr}
+    params = {'Qi': b_Qi, 'Qc': b_Qc, 'theta': b_theta, 'fc': b_fc, 'phi': b_phi}
 
-    return Parametric1D(expr, params, call_type=pd.Series)
+    return Parametric1D(expr, params, call_type=xr.DataArray)
 
 def fit_reflection(s11, k=50):
     """ fit the resonance parameters for a notch resonator to the resonance s11
 
     Args:
-        s11:    The complex resonance represented as a fitkit.Signal1D type
+        s11:    The complex resonance represented as an xarray.DataArray type with
+                frequency axis.
         k:      The number of samples taken from both ends of the spectrum, to use
                 to apply a global phase offset to the data
 
     Returns:
         model:  The Parametric1D model of the fitted resonance
     """
-    pm = ideal_reflection(b_fr=(np.min(s11.index), np.mean(s11.index), np.max(s11.index)))
+    freq = s11.frequency.values
+    pm = ideal_reflection(b_fc=(np.min(freq), np.mean(freq), np.max(freq)))
     circle, _ = circle_fit(s11)
 
-    pm.v['fr'] = (s11).abs().idxmin()
-    pm.v['theta'] = np.angle(1 - circle.z) # use the circle tilt to get theta
+    pm['fc'] = s11.frequency[np.abs(s11).argmin()].item()
+    pm['phi'] = np.angle(1 - circle.z) # use the circle tilt to get phi
 
-    # when f = fr, |s11| = |(Kc - Ki)/(Kc + Ki)| = |1 - 2 Ki/(Kc + Ki)| ~ 1 - 2*r
+    # when f = fc, |s11| = |(Kc - Ki)/(Kc + Ki)| = |1 - 2 Ki/(Kc + Ki)| ~ 1 - 2*r
     # ==> Ki ~ r*Kl
     # ==> Qi ~ Ql/r
     # ==> Re(Qc) ~ 1/(1/Ql - 1/Qi) = Qi/(1/r - 1)
-    
-    Ql = pm.v['fr']/fwhm(s11) # the fwhm estimates a good starting point
+
+    Ql = pm['fc']/fwhm(s11) # the fwhm estimates a good starting point
     Qi = Ql/circle.r
-    Qc = Qi/(1/circle.r - 1) / np.cos(pm.v['theta'])
+    Qc = Qi/(1/circle.r - 1) / np.cos(pm['phi'])
     if Qc < 0:
         warnings.warn('attempted to set negative Qc')
-    pm.v.set('Qi', np.log10(Qi), clip=True)
-    pm.v.set('Qc', np.log10(np.abs(Qc)), clip=True)
+    pm.set('Qi', Qi, clip=True)
+    pm.set('Qc', np.abs(Qc), clip=True)
 
     def metric(y1, y2):
-        return ((np.real(y1) - np.real(y2))**2).sum() + \
-               ((np.imag(y1) - np.imag(y2))**2).sum()
+        return (((np.real(y1.values) - np.real(y2.values))**2).sum() + \
+                ((np.imag(y1.values) - np.imag(y2.values))**2).sum())
 
-    pm.freeze(['Qi', 'Qc', 'fr', 'theta'])
-    pm.fit(pd.concat([s11.iloc[:k], s11.iloc[-k:]]), metric=metric)
+    pm.freeze(['Qi', 'Qc', 'fc', 'phi'])
+    pm.fit(xr.concat([s11[:k], s11[-k:]], 'frequency'), metric=metric)
 
-    s11_narrow = s11.loc[pm.v['fr'] - 2*fwhm(s11):pm.v['fr'] + 2*fwhm(s11)].iloc[::2]
+    s11_narrow = s11.loc[pm['fc'] - 2*fwhm(s11):pm['fc'] + 2*fwhm(s11)][::2]
 
-    pm.freeze('phi')
-    pm.unfreeze(['Qi', 'Qc', 'fr', 'theta'])
-    pm.fit(s11_narrow, metric=metric)
+    pm.freeze('theta')
+    pm.unfreeze(['Qi', 'Qc', 'fc', 'phi'])
+    opt_result = pm.fit(s11_narrow, metric=metric)
     pm.unfreeze('phi')
 
     return pm
