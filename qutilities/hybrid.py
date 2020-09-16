5b9826f08d25a254e0599365c42ef1fb1ff47322
Daniel Parker <danielparker@live.com.au>
2020-09-16 14:43:11 +1000

    Improve hybrid resonance model
    Changin the parameter f_b to df simplifies interacting with the model as
    it represents the detuning between the cavity and the cavity + drive
    frequencies. Changing 'df' has the effect of translating the hybrid
    peaks in frequency, where as before changing f_b had a non-trivial
    effect on the magnitude response.
    
    This change also specifies the complex coupling quality factor phase in
    terms of radians to be consistent with the other resonance models in
    qutilities.

--- qutilities/hybrid.py
+++ qutilities/hybrid.py
@@ -8,6 +8,7 @@ models and tools for fitting hybridised resonances
 import warnings
 
 import numpy as np
+import xarray as xr
 import sympy as sp
 
 from  qutilities import *
@@ -16,10 +17,10 @@ from fitkit import *
 
 def hybrid_reflection(b_fa = (1e9, 8e9, 40e9),
                       b_fd = (-40e9, 15e9, 40e9),
-                      b_fb = (1e9, 20e9, 40e9),
+                      b_df = (-100e6, 0, 100e6),
                       b_Ki = (2, 3, 8),
                       b_Kc = (2, 3, 8),
-                      b_theta = (-180, 0, 180),
+                      b_theta = (-np.pi, 0, np.pi),
                       b_gamma = (2, 3, 8),
                       b_g = (2, 3, 8)):
     """ reflection model for a hybridized optomechanical resonance """
@@ -29,19 +30,18 @@ def hybrid_reflection(b_fa = (1e9, 8e9, 40e9),
         'Ki':    b_Ki,     # the log10 of the internal loss rate of the cavity
         'gamma': b_gamma,  # the log10 of the total loss rate of coupled mode
         'g':     b_g,      # the log10 of the coupling rate between the cavity and coupled mode
-        'f_d':   b_fd,     # the frequency of the drive (in Hz)
-        'f_b':   b_fb,     # the frequency of the coupled mode (in Hz)
         'f_a':   b_fa,     # the frequency of the cavity (in Hz)
+        'df':    b_df,     # the frequency offset of the coupled mode (in Hz)
     }
 
     K, Kc, theta, Ki, gamma, g = sp.symbols('K, Kc theta Ki gamma g')
-    f, f_d, f_b, f_a = sp.symbols('f f_d f_b f_a')
+    f, f_d, f_b, f_a, df = sp.symbols('f f_d f_b f_a df')
 
-    s11 = -1 - 1j*Kc*sp.exp(1j*np.pi*theta/180)*(1j*gamma/2 + 2*np.pi*(f - f_d - f_b))/ \
+    s11 = -1 - 1j*Kc*sp.exp(-1j*theta)*(1j*gamma/2 + 2*np.pi*(f - f_d - f_b))/ \
           (10**(2*g) - (1j*K/2 + 2*np.pi*(f - f_a))*(1j*gamma/2 + 2*np.pi*(f - f_d - f_b)))
-    expr = s11.subs(K, Ki + Kc*sp.exp(1j*np.pi*theta/180))
+    expr = s11.subs(K, Ki + Kc*sp.exp(-1j*theta)).subs(f_b, f_a - f_d + df)
 
     # convert rates into log units
     expr = expr.subs(Kc, 10**Kc).subs(Ki, 10**Ki).subs(gamma, 10**gamma)
 
-    return Parametric1D(expr, params)
+    return Parametric1D(expr, params, call_type=xr.DataArray)
