1a12f1b6b171e92205471682dd01a10f2efb66d1
Daniel Parker <danielparker@live.com.au>
2020-11-02 14:21:43 +1100

    Fix reflection model for DPA
    In this commit I make some style improvments, but the significant change
    is that I remove 'lambda' from the numerator of the reflection
    expression. This misplaced component comes from the idler and is offset
    in frequency if w != wp/2. This makes a huge difference to the fits. I
    have checked that if lambda -> 0 we get the standard reflection model
    back.

--- qutilities/dpa.py
+++ qutilities/dpa.py
@@ -8,45 +8,41 @@ models and tools for fitting the resonance parameters degenerate parametric ampl
 import warnings
 
 import numpy as np
-import xarray as xr
+import pandas as pd
 import sympy as sp
 
-from  qutilities import *
+from fitkit import Parametric1D
 
-from fitkit import *
-from fitkit.decimate import *
-
-def ideal_dpa_reflection(b_Qi = (10, 1000, 1000000),
-                         b_Qc = (10, 1000, 1000000),
-                         b_fc = (1e9, 7e9, 10e9),
-                         b_fp = (10e9, 15e9, 20e9),
-                         b_lamda = (1e4, 1e7, 1e10),
-                         b_phi = (-np.pi, 0, np.pi),
-                         b_theta = (-np.pi, 0, np.pi)):
+def ideal_dpa_reflection(b_Qi=(2, 3, 6),
+                         b_Qc=(2, 3, 6),
+                         b_fc=(1e9, 7e9, 10e9),
+                         b_fp=(10e9, 15e9, 20e9),
+                         b_lamda=(1e4, 1e7, 1e10),
+                         b_phi=(-np.pi, 0, np.pi),
+                         b_theta=(-np.pi, 0, np.pi)):
     """ returns a Parametric1D model for an ideal DPA in reflection
 
     Params:
-        Qi:     The log10 of the internal Q
-        Qc:     The log10 of the external Q
+        Qi:     The log10 of the internal quality factor
+        Qc:     The log10 of the modulus of the complex coupling quality factor
         fc:     The cavity frequency (in Hz)
         fp:     The pump frequency (in Hz)
-        lamda:  The log10 of the DPA non-linearity (in Hz)
-        phi:    The argument of the DPA non-linearity
+        lamda:  The DPA non-linearity (in Hz)
+        phi:    The argument of the coupling quality factor (in rads)
+        theta:  The phase offset factor (in rads)
 
     Args:   Parameter bounds as required by Parametric1D
     """
-    Ki, Qi, Kc, Qc, fp, fc, f, delta, lamda, phi, theta, varphi, fudge = \
-        sp.symbols('Ki Qi Kc Qc fp fc f delta lamda phi theta varphi fudge', real=True)
+    Ki, Qi, Kc, Qc, fp, fc, f, delta, lamda, phi, theta, varphi = \
+        sp.symbols('Ki Qi Kc Qc fp fc f delta lamda phi theta varphi', real=True)
 
-    # TODO : [real(Kc) + Ki] OR [Kc + Ki]? read Khalil more closely
-    s11 = ( Kc*(Kc + Ki)/2 + 1j*Kc*(delta + f + lamda) ) /\
+    s11 = ( Kc*(Kc + Ki)/2 + 1j*Kc*(delta + f) ) /\
           ( delta**2 + ((Kc + Ki)/2 + 1j*f)**2 - lamda**2 ) - 1
 
     expr = s11.subs(f, f - fp/2)\
-              .subs(Kc, fc/(Qc*sp.exp(1j*phi)))\
-              .subs(Ki, fc/Qi)\
-              .subs(delta, fc - fp/2)\
-              .subs(lamda, lamda)
+              .subs(Kc, fc/(10**(Qc)*sp.exp(1j*phi)))\
+              .subs(Ki, fc/(10**Qi))\
+              .subs(delta, fc - fp/2)
     expr *= sp.exp(-1j*theta)
 
     params = {
